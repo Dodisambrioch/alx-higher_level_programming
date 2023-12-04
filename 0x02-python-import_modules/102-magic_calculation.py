e7413e4e214c2451271dc0e041c98c7224f0767a
Dodisambrioch <dundosambrioch@gmail.com>
2023-12-04 09:50:41 -0800

    correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-import easy_print
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+         c = add(a, b)
+         for i in range(4, 6):
+             c = add(c, i)
+         return c
+    else:
+        return sub(a, b)
+    return 0
+
