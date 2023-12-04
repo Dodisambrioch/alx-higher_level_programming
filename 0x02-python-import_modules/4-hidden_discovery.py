78ba9ef8bbbac049783518264c92ede98cba9915
Dodisambrioch <dundosambrioch@gmail.com>
2023-12-04 08:06:51 -0800

    everything can be imported

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ "" "__main__":
+    """print all hidden directories"""
+    import hidden_4
+
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
